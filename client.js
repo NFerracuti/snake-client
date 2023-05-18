b69c15ee34b0c9309c3f9daeaa0416d08797be8b
NFerracuti <npferracuti@gmail.com>
2023-05-18 17:30:22 -0400

    added constants

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require('net');
+const { IP, PORT } = require("./constants");
 // establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host: "localhost",
-    port:  50541
+    IP,
+    PORT
   });
 
   // interpret incoming data as text
@@ -14,6 +15,9 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Connected to the game server");
     conn.write("Name: Nik");
+    conn.write("Say: silly snake goes sssssssss");
+    conn.write("Say: i'm a hungry lil snek");
+    conn.write("Say: sssssssss");
     // Add other connect-related actions here
   });
 
---
+++ constants.js
@@ -0,0 +1,8 @@
+const IP = 'local host';
+
+const PORT = 50541;
+
+module.exports = {
+  IP,
+  PORT,
+};
