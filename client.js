d632f854bf421569ba8d795aea2e7c8fb1734a1c
NFerracuti <npferracuti@gmail.com>
2023-05-18 13:38:08 -0400

    added connection messages to client

--- client.js
+++ client.js
@@ -6,10 +6,14 @@ const connect = function () {
     port:  50541
   });
 
-  conn.on("data", (data) => console.log(data));
-  
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on("data", (data) => console.log(data));
+
+  conn.on("connect", (connect) => console.log("Connected to the game server"));
+
+  conn.on("connect", () => conn.write("Name: NPF"));
 
   return conn;
 };
